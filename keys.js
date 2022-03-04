2a38691a5be200eda7ee4da04bac7cd610f82ac2
anastassiya.savostina <a.savostina@icloud.com>
2022-03-03 21:04:10 -0700

    done

--- keys.js
+++ keys.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 // var keys = require("./keys.js");
 console.log('this is loaded');
 
-=======
-console.log("This is loaded");
->>>>>>> 44c062402b83d9df353eda2948b892850465d1e6
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
--- liri.js
+++ liri.js
@@ -1,132 +1,73 @@
-<<<<<<< HEAD
-//read and set enviroment variable
-require("dotenv").config();
-var keys = require("./keys.js");
-var fs = require("fs");
-var axios = require("axios");
-var moment = require("moment");
-
-//user input
-var command = process.argv[2]
-var input = process.argv[3]
-var bandsApi = keys.apiKeys.bands;
-var omdbApi = keys.apiKeys.omdb;
-
-//movie this//
-function getMovie(input) {
-    console.log("inside movie-this")
-    axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy" + omdbApi)
-    .then(function (response) {
-
-    var moviedata = response.data;
-  
-    console.log("Title " + moviedata.Title)
-    console.log("Year " + moviedata.Year)
-    console.log("Rated " + moviedata.Rated)
-    console.log("Director " + moviedata.Director)
-    console.log("Actors " + moviedata.Actors)
-})
-}
-
-function findSong(input) {
-    console.log("Inside spotify-this-song")
-    //Launch spotify
-    keys.spotify.search({ type: 'track', query: input }, function (err, data) {
-        if (err) {
-            return console.log('Error Occurred' + err);
-        } else {
-        var spotifyArr = data.tracks.items;
-        // console.log(data.tracks.item[0])
-        for (i = 0; i < 2; i++) {
-            console.log("song name: " + spotifyArr[i].name)
-            console.log("artist: " + spotifyArr[i].artist[0].name)
-            console.log("------------------")
-        }
-    }
-    })
-}
-
-=======
-
-require("dotenv").config();
-var keys =require("./keys.js");
-var fs =require("fs");
-var axios=require("axios");
-var moment =require("moment");
-
-var command =process.argv[2, 3, 4]
-var input =process.argv[2, 3, 4]
-var bandApi = keys.apiKeys.bands;
-var omdbApi =keys.apiKeys.omdb;
-
 //Ana's part
-
-function getMovie(input) {
-    console.log("inside movie this");
-
- axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=full&tomatoes=true&apikey=6c3f9ab7&i=tt0118971" + omdbApi)
- .then (function(response) {
-var moviedata = response.data;
-
-//      function(response) {
-         console.log("Title of the movie: " + response.data.Title);
-         console.log("Year the movie came out: " + response.data.Year);
-         console.log("IMDB Rating of the movie: " + response.data.Rated);
-         console.log("Rotten Tomatoes Rating of the movie: " + response.data.RottenTomatoes);
-         console.log("Country where the movie was produced: " + response.data.Country);
-         console.log("Language of the movie: " + response.data.Language);
-         console.log("Plot of the movie: " + response.data.Plot);
-         console.log("Actors in the movie: " + response.data.Actors);
-     })
-    }
-
-
-require(".env").config();
-const axios= require("axios");
+require("dotenv").config();
 var keys = require("./keys.js");
+
+
+
+// Info for Moment -- Date Formatting
 var moment = require("moment");
-//movie this 
-function getMovie(input) {
-    console.log("inside movie-this")
-    axios.get("http://www.omdbapi.com/?t="+input+"&y=&plot=short&apikey="+ omdbApi)
 
-    .then(function(response){
-        var moviedata = response.data;
-        console.log("title: " + moviedata.Title)
-        console.log("year: "+ moviedata.Year)
-        console.log("rated: "+moviedata.Rated)
-        console.log("released: "+moviedata.Released)
-        console.log("director: "+ moviedata.Director)
-        console.log("writer: "+ moviedata.Writer)
-        console.log("actor: "+ moviedata.Actor)
-        console.log("plot: " + moviedata.Plot)
-    })
+// Info for fs
+var fs = require("fs");
+
+//Info for axios
+var axios = require("axios");
+
+// Takes command
+
+var command = process.argv[2]
+// var input = process.argv[2]
+// var bandApi = keys.apiKeys.bands;
+var omdbApi = keys.apiKeys.omdb;
+// var movie = response.data;
+var movie = process.argv[3];
+
+function movieThis(){
+if (movie == null) {
+    axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy")
+   
+    
+    .then(function (response) {
+        console.log(response)   
+        // var movie = response.data;
+    console.log("Title of the movie: " + response.data.Title);
+    console.log("Year the movie came out: " + response.data.Year);
+    console.log("IMDB Rating of the movie: " + response.data.Rated);
+    console.log("Rotten Tomatoes Rating of the movie: " + response.data.RottenTomatoes);
+    console.log("Country where the movie was produced: " + response.data.Country);
+    console.log("Language of the movie: " + response.data.Language);
+    console.log("Plot of the movie: " + response.data.Plot);
+    console.log("Actors in the movie: " + response.data.Actors);
+}) 
+                 
+}
+else {
+    const movieName = process.argv[3]
+    const queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
+    axios.get(queryUrl)
+        .then(function (response) {
+            console.log("Title of the movie: " + response.data.Title);
+            console.log("Year the movie came out: " + response.data.Year);
+            console.log("IMDB Rating of the movie: " + response.data.Rated);
+            console.log("Rotten Tomatoes Rating of the movie: " + response.data.RottenTomatoes);
+            console.log("Country where the movie was produced: " + response.data.Country);
+            console.log("Language of the movie: " + response.data.Language);
+            console.log("Plot of the movie: " + response.data.Plot);
+            console.log("Actors in the movie: " + response.data.Actors);
+}) 
+                 
+}
 }
 
-//spotify this song
-function findsong(input) {
-    console.log("inside find-song")
-    spotify.search({type: 'track', query: input }, function (err, data) {
-        if (err) {
-            return console.log('Error Occurred'+ err);
-        }
-        var spotifyArr = data.tracks.items;
-        //console.log(data.tracks.items[0])
-        for (i =0; i < 2; i++) {
-            console.log("song name: " + spotifyArr[i].artists[0].name)
-            console.log("-------")
-        }
 
-    })
-}
->>>>>>> 44c062402b83d9df353eda2948b892850465d1e6
-function startProg(command, input) {
+
+// function startProg(command, input) {
     switch (command) {
         case "concert-this": showConcert(input);
             break;
         case "spotify-this-song": findSong(input);
             break;
-        case "movie-this": getMovie(input);
+        case "movie-this": movieThis();
             break;
         case "do-what-it-says":
             console.log("do what it says");
@@ -135,20 +76,12 @@ function startProg(command, input) {
         default:
             console.log("LIRI doesn't know what you are talking about");
     }
- 
- 
-}
-startProg(command, input);
 
 
-<<<<<<< HEAD
+// }
+// startProg(command, input);
 
 
 
 
-//spotify//
-// var Spotify = require("node-spotify-api");
-// const { response } = require("express");
-// var spotify = new keys.spotify(keys.spotify);
-=======
->>>>>>> 44c062402b83d9df353eda2948b892850465d1e6
+
